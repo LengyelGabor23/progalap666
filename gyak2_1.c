f1c76cabcb4a714bd9cf639c54d934c6699f4991
LengyelGabor23 <113032531+LengyelGabor23@users.noreply.github.com>
2022-09-14 15:25:45 +0200

    Add files via upload

---
+++ gyak2_1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+
+    double a = 10.0;
+    int b = 3;
+    double result = 0;
+
+    printf("\nPélda");
+
+    result = a / b;
+    printf("\nSzámítás: %f / %d = %.2f", a, b, result);
+
+    return 0;
+}
---
+++ gyak_2_3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>>
+#define pi 3.14
+#include <math.h>
+
+int main() {
+
+    int r;
+    double k, t;
+
+    printf("\nAdja meg a kor sugarat: ");
+
+    scanf("%d", &r);
+
+    t = r*r*M_PI;
+    k = 2*M_PI*r;
+
+    printf("\nA kor terulete: %f\n", t);
+    printf("\nA kor kerulete: %f\n", k);
+
+
+}
